829116dd44b31ca54ae26fb3a4392f97d8641640
Ramona Häuselmann <ramona.haeuselmann@gmail.com>
2021-04-01 00:14:32 +0200

    add raytrace method using bresenham line drawing in 3d

--- planning3d/scripts/grid_map_3D.py
+++ planning3d/scripts/grid_map_3D.py
@@ -185,6 +185,77 @@ class GridMap3D:
 
         return marker
 
+    # Bresenham 3D
+    # https://www.geeksforgeeks.org/bresenhams-algorithm-for-3-d-line-drawing/
+    def raytrace(self, start, end):
+        ListOfPoints = []
+        ListOfPoints.append(start)
+        x1, y1, z1 = start
+        x2, y2, z2 = end
+        dx = abs(x2 - x1)
+        dy = abs(y2 - y1)
+        dz = abs(z2 - z1)
+        if (x2 > x1):
+            xs = 1
+        else:
+            xs = -1
+        if (y2 > y1):
+            ys = 1
+        else:
+            ys = -1
+        if (z2 > z1):
+            zs = 1
+        else:
+            zs = -1
+    
+        # Driving axis is X-axis"
+        if (dx >= dy and dx >= dz):        
+            p1 = 2 * dy - dx
+            p2 = 2 * dz - dx
+            while (x1 != x2):
+                x1 += xs
+                if (p1 >= 0):
+                    y1 += ys
+                    p1 -= 2 * dx
+                if (p2 >= 0):
+                    z1 += zs
+                    p2 -= 2 * dx
+                p1 += 2 * dy
+                p2 += 2 * dz
+                ListOfPoints.append((x1, y1, z1))
+    
+        # Driving axis is Y-axis"
+        elif (dy >= dx and dy >= dz):       
+            p1 = 2 * dx - dy
+            p2 = 2 * dz - dy
+            while (y1 != y2):
+                y1 += ys
+                if (p1 >= 0):
+                    x1 += xs
+                    p1 -= 2 * dy
+                if (p2 >= 0):
+                    z1 += zs
+                    p2 -= 2 * dy
+                p1 += 2 * dx
+                p2 += 2 * dz
+                ListOfPoints.append((x1, y1, z1))
+    
+        # Driving axis is Z-axis"
+        else:        
+            p1 = 2 * dy - dz
+            p2 = 2 * dx - dz
+            while (z1 != z2):
+                z1 += zs
+                if (p1 >= 0):
+                    y1 += ys
+                    p1 -= 2 * dz
+                if (p2 >= 0):
+                    x1 += xs
+                    p2 -= 2 * dz
+                p1 += 2 * dy
+                p2 += 2 * dx
+                ListOfPoints.append((x1, y1, z1))
+        return ListOfPoints
     # # use raytrace from introduction to robotics course 
     # def raytrace(self, start, end):
     #     """Returns all cells in the grid map that has been traversed
